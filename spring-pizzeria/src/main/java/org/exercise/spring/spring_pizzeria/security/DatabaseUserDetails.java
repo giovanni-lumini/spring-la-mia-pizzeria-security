faee6986b1d144525a9c73cf9fd8464c849849bf
giovanni-lumini <giovanni.lumini28@gmail.com>
2025-04-30 15:28:31 +0200

    DatabaseUserDetails

---
+++ spring-pizzeria/src/main/java/org/exercise/spring/spring_pizzeria/security/DatabaseUserDetails.java
@@ -0,0 +1,69 @@
+package org.exercise.spring.spring_pizzeria.security;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import org.exercise.spring.spring_pizzeria.model.User;
+import org.exercise.spring.spring_pizzeria.model.Role;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+public class DatabaseUserDetails implements UserDetails {
+
+    // variabili
+    private final Integer id;
+    private final String username;
+    private final String password;
+    private final Set<GrantedAuthority> authorities;
+
+    // costruttore
+    public DatabaseUserDetails(User user) {
+        this.id = user.getId();
+        this.username = user.getUsername();
+        this.password = user.getPassword();
+        this.authorities = new HashSet<GrantedAuthority>();
+
+        for (Role userRole : user.getRoles()) {
+            authorities.add(new SimpleGrantedAuthority(userRole.getName()));
+        }
+    }
+
+    // getter
+    public Integer getId() {
+        return this.id;
+    }
+
+    public String getUsername() {
+        return this.username;
+    }
+
+    public String getPassword() {
+        return this.password;
+    }
+
+    public Set<GrantedAuthority> getAuthorities() {
+        return this.authorities;
+    }
+
+    // da Userdetails per gestire lo stato dell'utente
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
